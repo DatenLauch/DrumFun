921944305670095e7eddd414eaa5d1bc2215aa89
DatenLauch <103434272+DatenLauch@users.noreply.github.com>
2023-04-20 04:15:14 +0200

    Added MIDI controller support (1/X)

---
+++ MMI AVT Project/Assets/Scripts/MidiScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MidiScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputSystem.onDeviceChange += (device, change) =>
+        {
+            if (change != InputDeviceChange.Added) return;
+            var midiDevice = device as Minis.MidiDevice;
+            if (midiDevice == null) return;
+
+            midiDevice.onWillNoteOn += (note, velocity) => {
+                Debug.Log(string.Format(
+                    "Note On #{0} ({1}) vel:{2:0.00} ch:{3} dev:'{4}'",
+                    note.noteNumber,
+                    note.noteNumber.GetType(),
+                    note.shortDisplayName,
+                    velocity,
+                    velocity.GetType(),
+                    (note.device as Minis.MidiDevice)?.channel,
+                    note.device.description.product
+                ));
+            };
+
+            midiDevice.onWillNoteOff += (note) => {
+                Debug.Log(string.Format(
+                    "Note Off #{0} ({1}) ch:{2} dev:'{3}'",
+                    note.noteNumber,
+                    note.shortDisplayName,
+                    (note.device as Minis.MidiDevice)?.channel,
+                    note.device.description.product
+                ));
+            };
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
