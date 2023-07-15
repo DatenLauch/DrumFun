1be193ba3855ee860c8c5bf8cde1367a330ab3e3
atJustinG <45684351+atJustinG@users.noreply.github.com>
2023-07-16 01:12:38 +0200

    triple spawn
    1st additional spawn(bugged) cause unknown

--- MMI AVT Project/Assets/Scripts/Spawner/SpawnerScript.cs
+++ MMI AVT Project/Assets/Scripts/Spawner/SpawnerScript.cs
@@ -7,6 +7,8 @@ public class SpawnerScript : MonoBehaviour
     public GameObject Note; // This is the prefab for the object you want to spawn
     public float speed = 10f; // This is the speed at which the object will move
 
+
+
     private void Start()
     {
         Application.targetFrameRate = 60;
@@ -14,14 +16,20 @@ public class SpawnerScript : MonoBehaviour
 
     void SpawnObject()
     {
+        
+
         // Instantiate the object prefab
-        GameObject newObject = Instantiate(Note, transform.position, transform.rotation);
+        GameObject midNote = Instantiate(Note, transform.position, transform.rotation);
+        GameObject leftNote = Instantiate(Note, transform.position=new Vector3(3.0f, 0.5f, -10.0f), transform.rotation);
+        GameObject rightNote = Instantiate(Note, transform.position=new Vector3(-3.0f, 0.5f, -10.0f), transform.rotation);
 
         // Get the rigidbody component of the new object
-        Rigidbody rb = newObject.GetComponent<Rigidbody>();
+        Rigidbody rbMid = midNote.GetComponent<Rigidbody>();
+        Rigidbody rbLeft = leftNote.GetComponent<Rigidbody>();
 
         // Set the velocity of the rigidbody to make the object move in a single direction
-        rb.velocity = transform.forward * speed;
+        rbMid.velocity = transform.forward * speed;
+        rbLeft.velocity = transform.forward * speed;
     }
 
     // Update is called once per frame
