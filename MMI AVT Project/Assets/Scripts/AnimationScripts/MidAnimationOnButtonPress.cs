d3256174327b30672d6526832f419b6548765c3a
atJustinG <45684351+atJustinG@users.noreply.github.com>
2023-07-21 13:37:19 +0200

    animation added
    all three drums play an animation on Button press. cleaned up Materials so they are not linked by an object(removing the object removes the materials on all other objects)

---
+++ MMI AVT Project/Assets/Scripts/AnimationScripts/MidAnimationOnButtonPress.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidAnimationOnButtonPress : MonoBehaviour
+{
+    public GameObject drum;
+    private Animator drumAnimator;
+
+    private void Start()
+    {
+        drumAnimator = drum.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!PauseMenu.isPaused)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Debug.Log("Blue!");
+                drumAnimator.Play("hitAnimation", -1, 0f);
+            }
+        }
+    }
+}
